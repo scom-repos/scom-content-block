ea6219b167c1836120052ca27571c1c1f3816724
MikeYuen <nhatminh.150596@gmail.com>
2023-04-04 15:58:04 +0700

    SCB-06 update test

---
+++ test/modules/main/index.tsx
@@ -0,0 +1,26 @@
+import {application, Container, customModule, Module, Panel} from '@ijstech/components';
+import ContentBlock from '@scom/scom-content-block';
+
+@customModule
+export class MainModule extends Module {
+  private contentBlock: ContentBlock;
+
+  constructor(parent?: Container, options?: any) {
+    super(parent, options);
+  }
+
+  init() {
+    super.init();
+    // if (this.options.data) this.contentBlock.setData(this.options.data);
+  }
+
+  render() {
+    return (
+      <i-panel>
+        <i-hstack id="mainStack" margin={{top: '1rem', left: '1rem'}} gap="2rem">
+          <i-scom-content-block id={'contentBlock'} width={'33%'} height={'auto'}></i-scom-content-block>
+        </i-hstack>
+      </i-panel>
+    );
+  }
+}
